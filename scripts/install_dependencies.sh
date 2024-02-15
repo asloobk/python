d574dec476cd67966f2f1d24e83a63a1e4d6a124
Asloob Khan <asloobkhan.444@gmail.com>
2024-02-15 13:53:59 +0500

    first commit22

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,5 +6,17 @@ apt-get update
 # Install Python and pip
 apt-get install -y python3 python3-pip
 
+# Install virtualenv
+pip3 install virtualenv
+
+# Change directory to the location of the Python app
+cd /home/ubuntu
+
+# Create a virtual environment
+virtualenv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
 # Install Python dependencies using pip
-pip3 install -r /var/www/html/requirements1.txt
+pip3 install -r /home/ubuntu/requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,8 @@
 # Change directory to the location of the Python app
 cd /home/ubuntu
 
+# Activate the virtual environment
+source venv/bin/activate
+
 # Start the Flask app
 python3 app.py
