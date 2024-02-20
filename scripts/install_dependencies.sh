08cacde937bd27d265d7bde1eeb926f60cbdb2b1
Asloob Khan <asloobkhan.444@gmail.com>
2024-02-20 16:57:26 +0500

    first commitchec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# Update package index
-apt-get update
-
-# Install Python and pip
-apt-get install -y python3 python3-pip
-
-# Install virtualenv
-pip3 install virtualenv
-
-# Change directory to the location of the Python app
-#!/bin/bash
-
-# Update package index
-apt-get update
-
-# Install Python and pip
-apt-get install -y python3 python3-pip
-
-# Install virtualenv
-pip3 install virtualenv
-
 # Change directory to the location of the Python app
 cd /home/ubuntu
+
+# Update package index
+apt-get update
+
+# Install Python and pip
+apt-get install -y python3 python3-pip
+
+# Install virtualenv
+pip3 install virtualenv
 
 # Create a virtual environment
 virtualenv /home/ubuntu/venv
@@ -32,4 +20,3 @@ source /home/ubuntu/venv/bin/activate
 
 # Install Python dependencies using pip
 pip3 install -r /home/ubuntu/requirements.txt
-
