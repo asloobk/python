361166839d4ea5415281200cf5766923112f6b4f
Asloob khan <147397491+asloobk@users.noreply.github.com>
2024-02-21 17:53:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     print("Handling request for /")
-    return 'Hello, World123jklo!'
+    return 'Hello, Rana Rehan !'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
