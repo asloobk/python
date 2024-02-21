01ae8d7a435cd9477bc4ab21abd26f0561ae0ac7
Asloob khan <147397491+asloobk@users.noreply.github.com>
2024-02-21 18:07:56 +0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,7 @@
- sudo systemctl restart my_flask_app.service
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/ec2-user/my_project/my_env/bin/activate
+
+# Start the Flask application service
+sudo systemctl start my_flask_app
