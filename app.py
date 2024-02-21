39a7144004dfadccd6072a7551704df15b7e43ee
Asloob Khan <asloobkhan.444@gmail.com>
2024-02-21 12:57:17 +0500

    first csos

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
