ebf10f49cc89d313b56895f3693264dee4e9aec7
Asloob khan <147397491+asloobk@users.noreply.github.com>
2024-02-21 16:22:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World1234!'
-
-@app.route('/about')
-def about():
-    return 'This is the about page.'
-
-@app.route('/contact')
-def contact():
-    return 'Contact us at example@example.com'
+    print("Handling request for /")
+    return 'Hello, World123jk!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
